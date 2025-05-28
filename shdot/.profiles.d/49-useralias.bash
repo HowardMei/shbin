333c0993d247c01467f082a0bbc7bc1b1cbadd8e
Howard Mei <howardleomei@gmail.com>
2025-05-28 19:37:15 +0800

    Fix shdot/.profiles.d/49-useralias.bash return error

--- shdot/.profiles.d/49-useralias.bash
+++ shdot/.profiles.d/49-useralias.bash
@@ -1,6 +1,6 @@
 set +o nounset     # Don't mess up the auto completion
 set +o errexit     # Don't mess up the interactive shell
-[ -z "${BASH_RC}" ] && return 
+## [ -z "${BASH_RC}" ] && return 
 
 # Define full featured bash aliaes and functions
 unalias -a
